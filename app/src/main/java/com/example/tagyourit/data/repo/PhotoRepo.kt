905e4cae00f9f04f21c84bef784771e862bd02ee
NicholasHeilman <Nicholas.Heilman@gmail.com>
2020-08-21 14:30:33 -0400

    add photo dir

--- app/src/main/java/com/example/tagyourit/data/repo/PhotoRepo.kt
+++ app/src/main/java/com/example/tagyourit/data/repo/PhotoRepo.kt
@@ -1,7 +1,7 @@
 package com.example.tagyourit.data.repo
 
 import com.example.tagyourit.data.api.PhotoService
-import com.example.tagyourit.data.model.SearchResponse
+import com.example.tagyourit.data.model.PhotoSearchResponse
 import retrofit2.Response
 import javax.inject.Inject
 
@@ -9,6 +9,6 @@ class PhotoRepo @Inject constructor(
     private val photoService: PhotoService
 ) {
 
-    suspend fun getPhotos() : Response<SearchResponse> =
-        photoService.getPhotos(1,1)
+    suspend fun getPhotos() : Response<PhotoSearchResponse> =
+        photoService.getPhotos(10,1)
 }
