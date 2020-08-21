8afe69afd5f5be768ed4f39e55ec268ec30e6c5e
nestor <nestor.nia@gmail.com>
2020-08-21 13:30:05 -0400

    Deleted RetrofitInstance and renamed entitites

--- app/src/main/java/com/example/tagyourit/ui/MainActivity.kt
+++ app/src/main/java/com/example/tagyourit/ui/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.tagyourit.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.example.tagyourit.R
+import com.example.tagyourit.databinding.ActivityMainBinding
+import com.example.tagyourit.di.AppModule
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(LayoutInflater.from(this)) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
     }
 }
